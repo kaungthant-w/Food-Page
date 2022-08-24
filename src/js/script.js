51ff57d41ef9cb2712d55f7483e7596653de1b51
kaungthant-w <kaungthant9513@gmail.com>
2022-08-24 23:44:21 +0630

    Download app

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,34 @@ open.addEventListener("click", function() {
 close.addEventListener("click", function() {
     nav.classList.remove("left-0");
 });
+
+
+//lightbox
+const lightbox = document.querySelector(".lightbox");
+// console.log(lightbox);
+const lightbody = document.querySelector(".lightbox-body");
+const images = document.querySelectorAll("#gallery");
+// console.log(images);
+
+
+images.forEach(function(image) {
+    image.addEventListener("click", function(){
+        lightbox.classList.add("scale-100");
+        const img = document.createElement("img");
+        img.src = image.src;
+        img.classList.add("w-full");
+        img.classList.add("h-full");
+        img.classList.add("image-cover");
+        if(lightbody.children[0]) {
+            lightbody.removeChild(lightbody.children[0])
+        }
+        // console.log(img);
+
+        lightbody.appendChild(img);
+        // console.log(lightbody);
+    })
+});
+
+lightbox.addEventListener("click", function(){
+    lightbox.classList.remove("scale-100");
+})
